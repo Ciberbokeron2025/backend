df4b4d5466f351500fde56f2949a1d37ab14ec9b
Pablo Ferreiro <me@pabloferreiro.es>
2025-10-16 18:19:05 +0200

    api ready

--- src/Api.php
+++ src/Api.php
@@ -2,9 +2,23 @@
 
 namespace App;
 
+use GuzzleHttp\Client;
+
 class Api
 {
     private const string BASE_URL = "";
+
+    private Client $client;
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => self::BASE_URL,
+            // You can set any number of default request options.
+            'timeout' => 2.0,
+        ]);
+    }
 
     public static function login(): bool
     {
